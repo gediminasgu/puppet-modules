eadf78982f405f7ac42ebac3e328fe1e0dcfa326
Gediminas <fzr600@gmail.com>
2012-05-19 13:56:10 +0300

    Changed tomcat version

--- site.pp
+++ site.pp
@@ -1,4 +1,5 @@
-# mv site.pp /etc/puppet/manifests/
+# cp site.pp /etc/puppet/manifests/
+# puppet apply /etc/puppet/manifests/site.pp
 node default {
   include java
   include activemq
--- tomcat/manifests/init.pp
+++ tomcat/manifests/init.pp
@@ -1,18 +1,20 @@
-class tomcat {
+class tomcat(
+	$package = "apache-tomcat-7.0.27"
+) {
     exec { "download_tomcat":
-        command => "/usr/bin/wget http://apache.mirror.vu.lt/apache/tomcat/tomcat-7/v7.0.25/bin/apache-tomcat-7.0.25.tar.gz",
+        command => "/usr/bin/wget http://apache.mirror.vu.lt/apache/tomcat/tomcat-7/v7.0.25/bin/$package.tar.gz",
         cwd => "/usr/local",
-        creates => "/usr/local/apache-tomcat-7.0.25.tar.gz"
+        creates => "/usr/local/$package.tar.gz"
     }
     exec {"unzip_tomcat":
-        command => "/bin/tar zxvf apache-tomcat-7.0.25.tar.gz",
+        command => "/bin/tar zxvf $package.tar.gz",
         cwd => "/usr/local",
-        creates => "/usr/local/apache-tomcat-7.0.25",
+        creates => "/usr/local/$package",
         require => [Exec["download_tomcat"]]
     }
 	file { '/usr/local/tomcat':
 	   ensure => 'link',
-	   target => '/usr/local/apache-tomcat-7.0.25',
+	   target => '/usr/local/$package',
 	}
 	file {'tomcat-service':
 		path => "/etc/init.d/tomcat",
