d462316530c78c8536fca32c353acaadc60da4ad
Gediminas <fzr600@gmail.com>
2012-05-20 16:15:35 +0300

    mongodb and mysql added

--- site.pp
+++ site.pp
@@ -1,9 +1,17 @@
+# git submodule add git://github.com/duritong/puppet-mysql.git
 # cp site.pp /etc/puppet/manifests/
 # puppet apply /etc/puppet/manifests/site.pp
-node default {
+node default inherits basenode {
+  include mongodb
   include java
   include activemq
   include tomcat
   include jetty
   include mule
+
+  include mysql
+  class { 'mysql::server':
+    config_hash => {'root_password' => $mysql_root_pw}
+  }
+
 }
