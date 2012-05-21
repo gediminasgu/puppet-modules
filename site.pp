9463d44ec9f4042a6bbcc73c45cbe20083ce9dc8
Gediminas <fzr600@gmail.com>
2012-05-21 08:26:00 +0300

    nginx added

--- site.pp
+++ site.pp
@@ -1,6 +1,8 @@
-# git submodule add git://github.com/puppetlabs/puppetlabs-mysql.git
-# mv puppetlabs-mysql mysql
-# ln site.pp /etc/puppet/manifests/site.pp
+# git submodule add git://github.com/puppetlabs/puppetlabs-mysql.git /etc/puppet/modules/mysql
+# git submodule add git://github.com/garthk/puppet-rabbitmq.git /etc/puppet/modules/rabbitmq
+# git submodule add git://github.com/BenoitCattie/puppet-nginx.git /etc/puppet/modules/nginx
+# git submodule add git://github.com/BenoitCattie/puppet-php5-fpm.git /etc/puppet/modules/php5-fpm
+# ln -s /etc/puppet/modules/site.pp /etc/puppet/manifests/site.pp
 # puppet apply /etc/puppet/manifests/site.pp
 
 import 'basenode.pp'
@@ -11,6 +13,7 @@ node default inherits basenode {
   include tomcat
   include jetty
   include mule
+  include rabbitmq
 
   include mysql
   class { 'mysql::server': }
@@ -20,4 +23,18 @@ node default inherits basenode {
     host     => 'localhost',
     grant    => ['all'],
   }
+  package { 'php5-mysql': ensure => present }
+
+  include php5-fpm
+  include nginx
+  include nginx::fcgi
+  nginx::fcgi::site {"default":
+	root            => "/var/www/joomla",
+	fastcgi_pass    => "127.0.0.1:9000",
+	server_name     => ["localhost", "$hostname", "$fqdn"],
+  }
+  
+  package { 'zip': ensure => present }
+  package { 'unzip': ensure => present }
+  package { 'sendmail': ensure => present }
 }
