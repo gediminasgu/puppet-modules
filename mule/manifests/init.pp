b8ee790fde0abbfd58d7cd9a52c83c2b3d74640c
Gediminas <fzr600@gmail.com>
2012-05-26 22:33:44 +0300

    mule checks

--- mule/manifests/init.pp
+++ mule/manifests/init.pp
@@ -80,4 +80,13 @@ class mule{
 		ensure => present
 	}
 
+  puppi::check { 'MULE Error Check':
+    command => "check_jmx.sh -U service:jmx:rmi:///jndi/rmi://localhost:${mule::params::jmx_port}/jmxrmi -O 'Mule.amr:name=\"application totals\",type=Application' -A ExecutionErrors -w 0 -c 100",
+    hostwide => 'yes',
+  }
+
+  puppi::check { 'MULE Fatal Error Check':
+    command => "check_jmx.sh -U service:jmx:rmi:///jndi/rmi://localhost:${mule::params::jmx_port}/jmxrmi -O 'Mule.amr:name=\"application totals\",type=Application' -A FatalErrors -w 0 -c 3",
+    hostwide => 'yes',
+  }
 }
