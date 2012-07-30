08364477a7d2a1bd452ca46b6f5d5199a757b623
Gediminas <fzr600@gmail.com>
2012-07-30 23:10:59 +0300

    tomcat version upgrade

--- tomcat/manifests/init.pp
+++ tomcat/manifests/init.pp
@@ -1,5 +1,5 @@
 class tomcat{
-	$version = "7.0.27"
+	$version = "7.0.29"
 	$package = "apache-tomcat-$version"
     exec { "download_tomcat":
         command => "/usr/bin/wget http://apache.mirror.vu.lt/apache/tomcat/tomcat-7/v$version/bin/$package.tar.gz",
